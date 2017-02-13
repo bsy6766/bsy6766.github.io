a7c4f1293cf054ef6577b53e2c7553bc8753fd69
Seung Youp Baek <Seung Youp Baek>
2017-02-13 02:35:15 -0600

    Working

--- js/slider.js
+++ js/slider.js
@@ -1,40 +1,74 @@
 var imageCount = 1;
+var projectName;
+var totalImageCount;
 var timer;
+var idName;
 var paused = false;
+var ended = true;
 
-function slider(offset, projectName, totalImageCount) {
-	var image = document.getElementById('image');
-	imageCount = imageCount + offset;
-	if(imageCount > totalImageCount)
+function slider(offset) {
+	if(!ended)
 	{
-		imageCount = 1;
-	}
-
-	if(imageCount < 1)
-	{
-		imageCount = totalImageCount;
-	}	
-
-	image.src = "img/projects/detail/" + projectName+ imageCount +".png";
-
-	clearTimeout(timer);
-	if(!paused)
-	{
-    	timer = setTimeout(function() { slider(1, projectName, totalImageCount);}, 3000);
-	}
-	
-	if(offset == 0)
-	{
-		if(paused)
+		var image = document.getElementById(idName);
+		imageCount = imageCount + offset;
+		if(imageCount > totalImageCount)
 		{
-			paused = false;
+			imageCount = 1;
+		}
+
+		if(imageCount < 1)
+		{
+			imageCount = totalImageCount;
+		}	
+
+		image.src = "img/projects/detail/" + projectName+ imageCount +".png";
+		// alert("img/projects/detail/" + projectName+ imageCount +".png");
+
+		clearTimeout(timer);
+		if(!paused)
+		{
 	    	timer = setTimeout(function() { slider(1, projectName, totalImageCount);}, 3000);
+		}
+		
+		if(offset == 0)
+		{
+			if(paused)
+			{
+				paused = false;
+		    	timer = setTimeout(function() { slider(1, projectName, totalImageCount);}, 3000);
+			}
 		}
 	}
 }
 
-function endSlider()
+function pauseSlider()
 {
 	clearTimeout(timer);
 	paused = true;
+	// alert("Pause slider, name: " + projectName);
+}
+
+function stopSlider()
+{
+	ended = true;
+	// alert("End slider, name: " + projectName);
+}
+
+function startSlider()
+{
+	ended = false;
+	// alert("Start slider, name: " + projectName);
+}
+
+function initSlider(index, _projectName, _totalImageCount, _idName)
+{
+	imageCount = index;
+	projectName = _projectName;
+	totalImageCount = _totalImageCount;
+	idName = _idName;
+	paused = false;
+
+	// alert("init with index: " + index + ", projectName: " + _projectName + ", totalImageCount: " + _totalImageCount);
+
+	slider(0);
 }
