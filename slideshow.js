7f1aaa71403cf78f3e3d2c0313a7d904af4bc83d
Seung Youp Baek <bsy6766@gmail.com>
2021-03-15 23:10:57 +0900

    More work on slideshow to work

--- slideshow.js
+++ slideshow.js
@@ -1,20 +1,16 @@
 var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+showSlides(slideIndex, 'MHWASSSlides');
+showSlides(slideIndex, 'MeteorTapSlides');
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n, slideName) {
+  showSlides(slideIndex = n, slideName);
 }
 
-function showSlides(n) {
+function showSlides(n, slideName) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName(String(slideName));
+  var dots = document.getElementsByClassName('dot');
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
