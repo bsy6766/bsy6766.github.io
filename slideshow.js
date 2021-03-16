1526ff5f88412fd9f78288789db09dd8f0d9c792
Seung Youp Baek <bsy6766@gmail.com>
2021-03-16 17:42:23 +0900

    Bugfix, Refined more pages

--- slideshow.js
+++ slideshow.js
@@ -1,4 +1,5 @@
 var slideIndex = 1;
+showSlides(slideIndex, 'BMTapSlides');
 showSlides(slideIndex, 'MHWASSSlides');
 showSlides(slideIndex, 'MeteorTapSlides');
 
@@ -10,6 +11,7 @@ function currentSlide(n, slideName) {
 function showSlides(n, slideName) {
   var i;
   var slides = document.getElementsByClassName(String(slideName));
+  if (slides.length == 0) return;
   var dots = document.getElementsByClassName('dot');
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
